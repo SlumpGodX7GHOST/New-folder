a0f877bbd0f802b6f4edf671e82c7b215c7ad1a7
sahilDahiya99 <115144377+SahilDahiya997@users.noreply.github.com>
2022-10-28 13:31:39 +0530

    ....

---
+++ express/src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+console.log(path.join(__dirname,"../public"));
+const staticPath = path.join(__dirname,"../public")
+app.use(express.static(staticPath))
+
+
+app.get('/', function(req, res) {
+  // res.send("helloo server");
+});
+
+app.listen(4000,()=>{
+
+  console.log('Server started at port 4000')
+});
