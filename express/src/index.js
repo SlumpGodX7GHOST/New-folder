68a8f1ab6519d3e7e3cd054cb3b6ebb11c456977
sahilDahiya99 <115144377+SahilDahiya997@users.noreply.github.com>
2022-10-31 13:30:21 +0530

    ..

--- express/src/index.js
+++ express/src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-console.log(path.join(__dirname,"../public"));
+// console. log(path.join(__dirname,"../public"));
 const staticPath = path.join(__dirname,"../public")
 app.use(express.static(staticPath))
 
